339fb84ab4d086589dfeda7033ad6f96f3fff0b6
Landon Waddell <Landon.waddell@gmail.com>
2021-05-20 22:05:40 -0600

    Added Time text 9AM - 5PM, added input section, added button. Working on local storage for input and color for rows based on if the time represented in the rown is past present or future

--- script.js
+++ script.js
@@ -15,26 +15,95 @@ function theDay() {
     var n = weekday[d.getDay()];
     document.getElementById("currentDay").innerHTML = n;
   }
+// set row color for pas present and future. 
+let row = $('.container');
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log(time);
+if (time <= $('#Oclock')){
+    console.log("ok");
+    // row.style.backgroundColor = "red";
+}
 
+for(let i = 9; i < 12; i++){
+    $("#table").append(` 
+        <div id="${i}-Oclock">
+        <div class="container">
+        <div class="row align-items-end">
+         <div class="time">
+${i}AM   
+         </div>
+        <div class="event">
+        <div class="input-group input-group-lg">
+  <span class="input-group-text" id="inputGroup-sizing-lg"></span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+</div>
+        </div>
+        <div class="submit">
+        <button type="button" class="btn btn-secondary btn-lg">Add Event</button>
+        </div>
+      </div>
+     </div>
+    `)    
+  }
+  for(let i = 12; i < 13; i++){
+    $("#table").append(` 
+        <div id="${i}-Oclock">
+        <div class="container">
+        <div class="row align-items-end">
+         <div class="time">
+${i}PM 
+         </div>
+        <div class="event">
+        <div class="input-group input-group-lg">
+        <span class="input-group-text" id="inputGroup-sizing-lg"></span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+      </div>
+        </div>
+        <div class="submit">
+        <button type="button" class="btn btn-secondary btn-lg">Add Event</button>
+        </div>
+      </div>
+     </div>
+    `)    
+  }
+  // generate 1PM
+  for(let i = 1; i < 6; i++){
+    $("#table").append(`
+        <div id="${i}-Oclock">
+        <div class="container">
+        <div class="row align-items-end">
+         <div class="time">
+${i}PM    
+         </div>
+        <div class="event">
+        <div class="input-group input-group-lg">
+        <span class="input-group-text" id="inputGroup-sizing-lg"></span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+      </div>
+        </div>
+        <div class="submit">
+        <button type="button" class="btn btn-secondary btn-lg">Add Event</button>
+                </div>
+      </div>
+     </div>
+    `)   
+  
+          
+  // save text input to local storage
 
-  function frame(){
-      let table = $('h3')
-for (let i = 12; i < table.length; i++) {
-    const element = array[i];
+  let eventAdd = $('.form-control');
+    let text = $('.text');
+    let button = $('button');
+  
+      eventAdd.on('input', letter => {
+          console.log(letter.target.value);
+          text.textContent = letter.target.value
+      })
+      const saveToLocalStorage = () => {
+          localStorage.setItem('textinput',text.textContent)
+      }
+
+      button.click(saveToLocalStorage).localStorage
+      //getItem('textinput',text.textContent) // this takes my 2PM to 5PM away
     }
-}
-var table = '';
-var rows = 12;
-for (let r = 0; r < rows.length; r++)
- {
-    table  += '<h3>'
-for (let c = 0; c < array.length; c++) 
-{
-    table += '<td>' + c + '</td>';
-    
-}
-    table += '</h3>'     
-}
-document.write('<table>' + table + '</table>')
-
-//todo need to learn how to append 
