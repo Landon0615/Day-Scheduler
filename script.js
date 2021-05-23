cc27db62cefd21109a3f9a438c55c986ceba998e
Landon Waddell <Landon.waddell@gmail.com>
2021-05-23 13:01:48 -0600

    Added local storage for events

--- script.js
+++ script.js
@@ -1,56 +1,75 @@
-
-
-const theDay = (moment().format("dddd, MMMM, Do"));
+const theDay = moment().format("dddd, MMMM, Do");
 $("#currentDay").text(theDay);
 
- const theHour = ((moment().format("H")))
- $("#currentDay").text(theDay);
- 
-let timeTextArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
-for(let i = 0; i < 9; i++){
-    let stringTop = `
-    <div class="row" id="${i+9}-Oclock">
+const theHour = moment().format("H");
+$("#currentDay").text(theDay);
+
+let timeTextArray = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+for (let i = 0; i < 9; i++) {
+  let stringTop = `
+    <div class="row" id="${i + 9}-Oclock">
         <p class="col-1 time">
         ${timeTextArray[i]}   
         </p>
-    `
-    let stringBottom =''
-    if(i+9< theHour){
-        stringBottom = `
-        <textarea class="col-10 past"> </textarea>
+    `;
+  let stringBottom = "";
+  if (i + 9 < theHour) {
+    stringBottom = `
+        <textarea class="col-10 past" id="${i + 9}-Oclock-text"> </textarea>
         <button class="col-1 saveBtn"> Save </button>
     </div>
-        `
-    }else if(i+9 == theHour){
-        stringBottom = `
-        <textarea class="col-10 present"> </textarea>
+        `;
+  } else if (i + 9 == theHour) {
+    stringBottom = `
+        <textarea class="col-10 present" id="${i + 9}-Oclock-text"> </textarea>
         <button class="col-1 saveBtn"> Save </button>
     </div>
-        `
-    }else {
-        stringBottom = `
-        <textarea class="col-10 future"> </textarea>
+        `;
+  } else {
+    stringBottom = `
+        <textarea class="col-10 future" id="${i + 9}-Oclock-text"> </textarea>
         <button class="col-1 saveBtn"> Save </button>
     </div>
-        `
-    }
-    $("#table").append(stringTop+stringBottom)    
+        `;
   }
-          
-  // save text input to local storage
+  $("#table").append(stringTop + stringBottom);
+}
 
-  let eventAdd = $('.form-control');
-    let text = $('.text');
-    let button = $('button');
-  
-      eventAdd.on('input', letter => {
-          console.log(letter.target.value);
-          text.textContent = letter.target.value
-      })
-      const saveToLocalStorage = () => {
-          localStorage.setItem('textinput',text.textContent)
-      }
+//   save text input to local storage
 
-      button.click(saveToLocalStorage).localStorage
-      getItem('textinput',text.textContent) // this takes my 2PM to 5PM away
-    console.log(localStorage)
+let eventAdd = $(".col-10");
+let text = {};
+let button = $("button");
+
+eventAdd.on("input", (eventText) => {
+  console.log(eventText.target.value);
+  text.textContent = eventText.target.value;
+  text.id = eventText.target.id;
+});
+const saveToLocalStorage = () => {
+  localStorage.setItem(text.id, text.textContent);
+};
+
+function init() {
+  $(document).ready(function () {
+    for (let i = 0; i < 9; i++) {
+      console.log(localStorage.getItem(`${i + 9}-Oclock-text`));
+      $(`#${i + 9}-Oclock-text`).val(localStorage.getItem(
+        `${i + 9}-Oclock-text`
+      ));
+    }
+  });
+}
+init();
+
+button.click(saveToLocalStorage);
